41e5f3c9ed8accf8301cd28f1a94ce43c3732bf7
Miller Nivia <millernivia1301@gmail.com>
2021-11-12 11:29:57 -0500

    12 Noviembre - Agenda y Evento Detalle

--- newapp/app/src/main/java/com/example/new_app/ui/EventoDetalleFragment.kt
+++ newapp/app/src/main/java/com/example/new_app/ui/EventoDetalleFragment.kt
@@ -6,6 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.new_app.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.MapView
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,25 +23,42 @@ private const val ARG_PARAM2 = "param2"
  * Use the [EventoDetalleFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class EventoDetalleFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class EventoDetalleFragment  : Fragment(), OnMapReadyCallback {
+    private var mapView: MapView? = null
+    private var googleMap: GoogleMap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_evento_detalle, container, false)
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_evento_detalle, container, false)
+
+        mapView = view?.findViewById(R.id.mapview) as MapView
+        mapView?.onCreate(savedInstanceState)
+        mapView?.getMapAsync(this)
+
+        return view
+    }
+    override fun onResume() {
+        mapView!!.onResume()
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView!!.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView!!.onLowMemory()
     }
 
     companion object {
@@ -57,4 +80,10 @@ class EventoDetalleFragment : Fragment() {
                 }
             }
     }
+
+    override fun onMapReady(p0: GoogleMap) {
+        googleMap?.addMarker(MarkerOptions().position(LatLng(0.0, 0.0)).title("Marker"))
+    }
+
+
 }
