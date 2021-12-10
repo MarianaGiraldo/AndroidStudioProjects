ee5a50252389c5c90b94deea59a56ff21debbf3e
MarianaGiraldo <mgiraldo594@misena.edu.co>
2021-12-10 09:53:53 -0500

    10 Diciembre - Splash

--- newapp/app/src/main/java/com/example/new_app/ui/SplashActivity.kt
+++ newapp/app/src/main/java/com/example/new_app/ui/SplashActivity.kt
@@ -1,12 +1,24 @@
 package com.example.new_app.ui
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.new_app.MainActivity
 import com.example.new_app.R
 
+private const val tiempo:Long=600
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout. activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, tiempo)
     }
 }
